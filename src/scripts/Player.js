7e85069f46a601f07588074229f6b54b632c6d16
Daksh <daksh.gupta@live.com>
2021-03-21 12:16:07 -0700

    Added collision detection and end point

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,13 +1,21 @@
-import { Object3D, Vector3 } from './three.min.js';
+import { Object3D, Raycaster, Vector3 } from './three.min.js';
 
 export default class Player extends Object3D {
 
-    constructor(camera) {
+    constructor(camera, maze) {
         super();
         this.dir = new Vector3();
-        this.maxSpeed = 0.3;
+        this.trueDir = new Vector3();
+        this.maxSpeed = 0.1;
         this.speed = 0;
+        this.ray = new Raycaster(
+            this.position,
+            this.trueDir,
+            0,
+            0.25
+        );
         this.camera = camera;
+        this.maze = maze;
 
         this.add(camera);
     }
@@ -17,9 +25,15 @@ export default class Player extends Object3D {
         this.camera.rotation.x = Math.max(-Math.PI/2, Math.min(this.camera.rotation.x, Math.PI/2));
 
         const angle = Math.atan2(this.dir.z, this.dir.x) + this.rotation.y - Math.PI/2;
-        const trueDir = new Vector3(-Math.sin(angle), 0, -Math.cos(angle));
+        this.trueDir.set(-Math.sin(angle), 0, -Math.cos(angle));
 
-        this.position.addScaledVector(trueDir, this.speed);
+        if(this.speed) {
+            const objs = this.ray.intersectObject(this.maze, true);
+            if(objs.length)
+                this.stop();
+        }
+
+        this.position.addScaledVector(this.trueDir, this.speed);
 
     }
     
