5936fa235e17729c8b006864173edbea1f6052af
Daksh <daksh.gupta@live.com>
2021-03-20 18:13:23 -0700

    Smoother movement and maze generation

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,37 @@
+import Maze from './Maze.js';
 import Player from './Player.js';
 import * as Three from './three.min.js';
 
 onresize = () => location.reload();
 
+const ui = document.getElementById('ui');
 const canvas = document.getElementById('main');
 canvas.width = innerHeight * 4/3;
 canvas.height = innerHeight;
 
 const renderer = new Three.WebGLRenderer({ canvas, antialias: true });
 
-const far = 1000;
-const camera = new Three.PerspectiveCamera(80, 4/3, 0.1, far);
+const camera = new Three.PerspectiveCamera(80, 4/3, 0.1, 1000);
 
 const scene = new Three.Scene();
 
-const light = new Three.DirectionalLight()
-    , light2 = new Three.DirectionalLight();
-light.position.set(-1, 2, 10);
-light2.position.set(1, -2, -10);
-
-const cube = new Three.Mesh(
-    new Three.BoxBufferGeometry(1, 1, 1),
-    new Three.MeshPhongMaterial({ color: 0x8c03fc })
-);
-
 const player = new Player(camera);
 
-scene.add(light);
-scene.add(light2);
-scene.add(cube);
-scene.add(player)
+const maze = new Maze();
+maze.position.set(0, -20, 0);
+
+scene.add(maze);
+scene.add(player);
 
 function render(time) {
 
     time *= 0.001;
     
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    ui.innerHTML = `
+        x: ${player.position.x.toFixed(2)}<br>
+        y: ${player.position.y.toFixed(2)}<br>
+        z: ${player.position.z.toFixed(2)}
+    `;
 
     player.render();
 
@@ -47,7 +42,10 @@ function render(time) {
 }
 requestAnimationFrame(render);
 
+let keys = new Set();
+
 onkeydown = e => {
+    if(keys.has(e.code)) return;
     switch(e.code) {
         case 'ArrowLeft':
         case 'KeyA':
@@ -68,11 +66,33 @@ onkeydown = e => {
         default:
             return;
     }
+    keys.add(e.code);
     player.start();
 }
 
 onkeyup = e => {
-    player.stop();
+    switch(e.code) {
+        case 'ArrowLeft':
+        case 'KeyA':
+            player.addDir(1, 0);
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            player.addDir(-1, 0);
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            player.addDir(0, 1);
+            break;
+        case 'ArrowUp':
+        case 'KeyW':
+            player.addDir(0, -1);
+            break;
+        default:
+            return;
+    }
+    keys.delete(e.code);
+    if(!keys.size) player.stop();
 }
 
 canvas.onclick = () => {
