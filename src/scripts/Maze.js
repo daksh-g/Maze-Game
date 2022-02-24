b89d8f9a93f0a1e3dd322701c7e2179f8c0759dd
Daksh <daksh.gupta@live.com>
2022-02-23 20:36:12 -0800

    Added respawning

--- src/scripts/Maze.js
+++ src/scripts/Maze.js
@@ -1,14 +1,14 @@
-import { DoubleSide, Mesh, MeshPhongMaterial, Object3D, PlaneBufferGeometry } from './three.min.js';
+import { DoubleSide, Mesh, MeshPhongMaterial, Object3D, PlaneGeometry } from './three.min.js';
 
 export default class Maze extends Object3D {
 
     constructor() {
         super();
-        this.n = 10;
+        this.size = 10;
         this.wallColor = 0x666699;
         this.wallWidth = 2;
         this.wallHeight = 1;
-        this.grid = Array.from(Array(this.n), () => new Array(this.n).fill(0));
+        this.grid = Array.from(Array(this.size), () => Array(this.size).fill(0));
         this.dirs = {
             N: 1,
             S: 2,
@@ -34,17 +34,12 @@ export default class Maze extends Object3D {
             W: 'E'
         };
 
-        this.generate(Math.random() * this.n | 0, Math.random() * this.n | 0);
+        this.generate();
         this.generateWalls();
-
-        this.addEdge(this.n / 2, 0);
-        this.addEdge(0, this.n / 2, true);
-        this.addEdge(this.n / 2, this.n);
-        this.addEdge(this.n, this.n / 2, true);
 
     }
 
-    generate(x, y) {
+    generate(x = Math.random() * this.size | 0, y = Math.random() * this.size | 0) {
 
         let dirs = ['N', 'S', 'E', 'W'];
 
@@ -58,9 +53,9 @@ export default class Maze extends Object3D {
             let nx = x + this.xdir[dir],
                 ny = y + this.ydir[dir];
             
-            if(nx < 0 || nx == this.n || ny < 0 || ny == this.n || this.grid[nx][ny])
+            if(nx < 0 || nx == this.size || ny < 0 || ny == this.size || this.grid[nx][ny])
                 continue;
-            
+       
             this.grid[x][y] |= this.dirs[dir];
             this.grid[nx][ny] |= this.dirs[this.odir[dir]];
 
@@ -72,7 +67,7 @@ export default class Maze extends Object3D {
 
     addEdge(x, y, rotated = false) {
         const edge = new Mesh(
-            new PlaneBufferGeometry(this.n * this.wallWidth),
+            new PlaneGeometry(this.size * this.wallWidth),
             new MeshPhongMaterial({ color: 0x000000, emissive: this.wallColor, side: DoubleSide})
         );
         edge.position.set(x * this.wallWidth, 0, y * this.wallWidth);
@@ -82,7 +77,7 @@ export default class Maze extends Object3D {
 
     addWall(x, y, dx, dy, rotated = false) {
         const wall = new Mesh(
-            new PlaneBufferGeometry(this.wallWidth, this.wallHeight),
+            new PlaneGeometry(this.wallWidth, this.wallHeight),
             new MeshPhongMaterial({ color: 0x000000, emissive: this.wallColor, side: DoubleSide })
         );
         wall.position.set(
@@ -95,18 +90,30 @@ export default class Maze extends Object3D {
     }
 
     generateWalls() {
-        for(let i = 0; i < this.n; i++)
-        for(let j = 0; j < this.n; j++) {
+        for(let i = 0; i < this.size; i++)
+        for(let j = 0; j < this.size; j++) {
 
             const elem = this.grid[i][j];
 
-            if(i != this.n - 1 && (elem & this.dirs.E) == 0)
+            if(i != this.size - 1 && (elem & this.dirs.E) == 0)
                 this.addWall(i + 1, j, 0, 0.5, true);
 
-            if(j != this.n - 1 && (elem & this.dirs.S) == 0)
+            if(j != this.size - 1 && (elem & this.dirs.S) == 0)
                 this.addWall(i, j + 1, 0.5, 0);
 
         }
+
+        this.addEdge(this.size / 2, 0);
+        this.addEdge(0, this.size / 2, true);
+        this.addEdge(this.size / 2, this.size);
+        this.addEdge(this.size, this.size / 2, true);
+    }
+
+    regenerate() {
+        this.children = [];
+        this.grid = Array.from(Array(this.size), () => Array(this.size).fill(0));
+        this.generate();
+        this.generateWalls();
     }
 
 }
